6e887e6ee4dd0f809b9341ce2dfbfa07bf71b29a
margo <margoshka.ri6@gmail.com>
2022-05-10 14:49:12 +0300

    Add project files.

---
+++ Detecting label/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Detecting_label
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.startBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(900, 453);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(407, 471);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(75, 23);
+            this.startBtn.TabIndex = 1;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(924, 504);
+            this.Controls.Add(this.startBtn);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button startBtn;
+    }
+}
---
+++ Detecting label/Form1.cs
@@ -0,0 +1,98 @@
+﻿using Amazon.Rekognition;
+using Amazon.Rekognition.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Image = Amazon.Rekognition.Model.Image;
+using Label = Amazon.Rekognition.Model.Label;
+
+namespace Detecting_label
+{
+    public partial class Form1 : Form
+    {
+        string path = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "png|*.png|jpeg|*.jpeg|jpg|*.jpg";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    path = ofd.FileName;
+                    this.pictureBox1.Image = System.Drawing.Image.FromFile(path);
+                    this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+                    this.startBtn.Enabled = false;
+                    Main();
+                }
+            }
+        }
+        public async Task Main()
+        {
+            string photo = path;
+
+            var awsCredentials = new Amazon.Runtime.BasicAWSCredentials("AKIA3BZMVBVKD2IHGKEZ", "rlTB6Ne7BJxDgX1nCOrMU98uDVdP8ux/VZkrgPqe");
+            var rekognitionClient = new Amazon.Rekognition.AmazonRekognitionClient(awsCreden‌tials, Amazon.RegionEndpoint.USEast1);
+
+            var image = new Amazon.Rekognition.Model.Image();
+            try
+            {
+                using var fs = new FileStream(photo, FileMode.Open, FileAccess.Read);
+                byte[] data = null;
+                data = new byte[fs.Length];
+                fs.Read(data, 0, (int)fs.Length);
+                image.Bytes = new MemoryStream(data);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed to load file " + photo);
+                return;
+            }
+            var detectlabelsRequest = new DetectLabelsRequest()
+            {
+                Image = image,
+                MaxLabels = 10,
+                MinConfidence = 75F,
+            };
+
+            try
+            {
+                DetectLabelsResponse detectLabelsResponse = await rekognitionClient.DetectLabelsAsync(detectlabelsRequest);
+                foreach (Label label in detectLabelsResponse.Labels)
+                {
+                    label.Instances.ForEach(instance => ShowBoundingBoxPositions(instance.BoundingBox));
+
+                    MessageBox.Show($"Name: {label.Name} Confidence: {label.Confidence}");
+                }
+                this.startBtn.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public void ShowBoundingBoxPositions(BoundingBox box)
+        {
+            float top = (box.Top * pictureBox1.Height);
+            float left = (box.Left * pictureBox1.Width);
+            float right = pictureBox1.Width * box.Width;
+            float bottom = pictureBox1.Height * box.Height;
+            Pen pen = new Pen(Color.Aqua, 2);
+            using (Graphics g = pictureBox1.CreateGraphics())
+            {
+                g.DrawRectangle(pen, left, top, right, bottom);
+            }
+        }
+
+    }
+}
